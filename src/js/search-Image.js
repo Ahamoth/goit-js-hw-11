d79523288ab3f021b56933791c5a20e437aad10b
Ahamoth <beeminer.store@gmail.com>
2023-01-26 17:23:32 +0200

    edit

--- src/js/search-Image.js
+++ src/js/search-Image.js
@@ -1,13 +1,10 @@
 import axios from "axios";
-
 const perPages = 40; 
 const KEY = '33017005-3089560dbf89e85a2a48421c2';
 const URL = 'https://pixabay.com/api/';
-
 export default async function loadImages(imageName, page) {
     try {
         return await axios.get(`${URL}?key=${KEY}&q=${imageName}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPages}`);
-
 } catch(err) {
   console.log(err);
 }
